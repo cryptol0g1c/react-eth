c272c5bf5b705fdd21dd84a867334db8a2b09b3a
af <agustinfiorenza@gmail.com>
2018-04-04 18:10:03 -0300

    fix builder

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'docs'),
     filename: 'bundle.js',
-    publicPath: './'
+    publicPath: '/react-eth/'
   },
   devtool: 'inline-source-map',
   plugins: [
