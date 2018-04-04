8e0a0ee4a95ec3dbade43a415849e365842039d8
af <agustinfiorenza@gmail.com>
2018-04-04 18:02:33 -0300

    build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'docs'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: './'
   },
   devtool: 'inline-source-map',
   plugins: [
