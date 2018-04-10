55117d46efc331abad02779dfb103dcb2d7a5bc5
af <agustinfiorenza@gmail.com>
2018-04-10 15:25:24 -0300

    improve demo

--- README.md
+++ README.md
@@ -9,6 +9,43 @@ The goal of this repository is to help developers to easily instantiate react fo
 ## Live demo
 https://afiorenza.github.io/react-eth/
 
+## Usage
+```
+import ReactEth from 'react-eth';
+
+const abi = {
+  "constant": false,
+  "inputs": [
+    {
+      "name": "spender",
+      "type": "address"
+    },
+    {
+      "name": "value",
+      "type": "uint256"
+    }
+  ],
+  "name": "approve",
+  "outputs": [
+    {
+      "name": "",
+      "type": "bool"
+    }
+  ],
+  "payable": false,
+  "stateMutability": "nonpayable",
+  "type": "function"
+};
+
+<ReactEth abi={abi}>
+```
+
+## Props
+* abi: json. This prop is the interface for abi json.
+* className: string.
+* onChange: function.
+* onSubmit: function.
+
 ## Examples
 To run examples:
 * Clone this repo
--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,11 @@
 require('./_index.css');
 
 import React from 'react';
+import ReactJson from 'react-json-view';
 import {render} from 'react-dom';
 import erc20Abi from './erc20abi';
 import ReactEth from '../src';
+import {isEmpty} from 'lodash';
 
 const rootDiv = document.createElement('div');
 document.body.appendChild(rootDiv);
@@ -12,17 +14,30 @@ let logEvent = (abi, event, formData) =>
   console.log(`Form ${abi.name}. Change ${event}. Form data: ${JSON.stringify(formData)}`)
 
 render(
-  <div>
-    <h1>react-eth</h1>
-    {
-      erc20Abi.map((abi, index) => <ReactEth {...{
-        abi,
-        className: 'react-eth-example',
-        key: index,
-        onChange: ({formData}) => logEvent(abi, 'change', formData),
-        onSubmit: ({formData}) => logEvent(abi, 'submit', formData)
-      }} />)
-    }
+  <div className="body">
+    <h1 className="header">react-eth</h1>
+    <div className="container">
+      {erc20Abi.map((abi, index) =>
+        <div className="row example-row" key={`example-${index}`}>
+          <div className="col">
+            <ReactEth {...{
+              abi,
+              className: 'react-eth-example',
+              onChange: ({formData}) => logEvent(abi, 'change', formData),
+              onSubmit: ({formData}) => logEvent(abi, 'submit', formData)
+            }} />
+          </div>
+          <div className="col">
+            {
+              !isEmpty(abi.inputs) && <ReactJson enableClipboard={false} src={abi} theme="monokai" style={{
+                maxHeight: '30rem',
+                overflowY: 'auto'          
+              }} />
+            }
+          </div>
+        </div>
+      )}
+    </div>
   </div>,
   rootDiv
 );
