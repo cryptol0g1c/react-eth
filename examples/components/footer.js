c1b060974f9ba224848683d07d0a2097165da601
af <agustinfiorenza@gmail.com>
2018-04-17 11:49:37 -0300

    add footer

---
+++ examples/components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export let Footer = () =>  <div className="footer">
+  Powered by <a href="https://github.com/afiorenza/react-eth" target="_blank">react-eth</a>
+</div>
+
+export default Footer;
--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ require('./_index.css');
 
 import React from 'react';
 import {render} from 'react-dom';
-import {ExampleGrid, Playground} from './components/'
+import {ExampleGrid, Footer, Playground} from './components/'
 import arraysAbi from './abis/arrays';
 import erc20Abi from './abis/erc20';
 
@@ -10,17 +10,20 @@ const rootDiv = document.createElement('div');
 document.body.appendChild(rootDiv);
 
 render(
-  <div className="body">
-    <div className="container">
-      <h1>ERC20</h1>
-      <ExampleGrid abi={erc20Abi} />
-      <br />
-      <h1>With arrays</h1>
-      <ExampleGrid abi={arraysAbi} />
-      <h1>Playground</h1>
-      <h3>Test your own ABI</h3>
-      <Playground />
+  <div>
+    <div className="body">
+      <div className="container">
+        <h1>ERC20</h1>
+        <ExampleGrid abi={erc20Abi} />
+        <br />
+        <h1>With arrays</h1>
+        <ExampleGrid abi={arraysAbi} />
+        <h1>Playground</h1>
+        <h3>Test your own ABI</h3>
+        <Playground />
+      </div>
     </div>
+    <Footer />
   </div>,
   rootDiv
 );
