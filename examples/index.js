6a7604d3f561dd7077dd83d67b79e3fe0d7c57bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-17 14:05:37 +0000

    Deploy afiorenza/react-eth to github.com/afiorenza/react-eth.git:gh-pages

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ require('./_index.css');
 
 import React from 'react';
 import {render} from 'react-dom';
-import ExampleGrid from './components/'
+import {ExampleGrid, Playground} from './components/'
 import arraysAbi from './abis/arrays';
 import erc20Abi from './abis/erc20';
 
@@ -17,6 +17,9 @@ render(
       <br />
       <h1>With arrays</h1>
       <ExampleGrid abi={arraysAbi} />
+      <h1>Playground</h1>
+      <h3>Test your own ABI</h3>
+      <Playground />
     </div>
   </div>,
   rootDiv
